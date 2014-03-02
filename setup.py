4f7f301d689eb64b2ce333c7796ce470a193dfdf
Siddharth Gupta <sids.1992@gmail.com>
2014-03-02 19:31:37 +0530

    Rekindled App
    - Added Facebook Support
    - Desktop app is stil lost
    
    - Handing over to Sriram

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(
-    name='YourAppName',
+    name='VITCloudDev',
     version='1.0',
     description='OpenShift App',
-    author='Your Name',
+    author='Siddharth Gupta',
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django>=1.3'],
+    install_requires=['Django<=1.4', 'oauth2>=1.5.167', 'python_openid>=2.2', 'selenium>=2.29.0', 'mock==1.0.1', 'django-social-auth'],
 )
